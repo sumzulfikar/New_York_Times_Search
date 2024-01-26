fa9c2743611ef2578699ddf9c6ea565677da6469
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-01-26 11:51:57 +0000

    Search text returns results

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ $( "#startDate" ).datepicker();
 $( "#endDate" ).datepicker();
 
 
-var APIkey="qTw8EMKwPhaPhhIorMAdyy6Av9WgQ8EE";
-var search="election";
-var queryurl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+"&api-key="+APIkey;
+
+$("#submit").on("click",searchArticles);
 
 
+function searchArticles(event){
+  var termSearch=$("#termSearch").val().trim();
+      console.log(termSearch);
+  var APIkey="qTw8EMKwPhaPhhIorMAdyy6Av9WgQ8EE";
+  var queryurl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+termSearch+"&api-key="+APIkey;
+
+
+  event.preventDefault();
 fetch(queryurl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+          
 
-console.log(data);
+    console.log(data);
 
 
 
     });
-    
+  }
